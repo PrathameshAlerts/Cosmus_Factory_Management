161ec93c60e1461555e9b566fa11a53c500b05d1
PrathameshAlert <alerts.prathamesh@gmail.com>
2024-02-03 17:55:49 +0530

    backend ledger tables

--- ERP_Cosmus/product/urls.py
+++ ERP_Cosmus/product/urls.py
@@ -55,5 +55,7 @@ urlpatterns = [
     path('unitnamedelete/<slug:slug>',views.unit_name_delete , name= 'unit_name-delete'),
 
     #common Routes
-    path('allmaster/', views.allmaster , name= 'all-master')
+    path('allmaster/', views.allmaster , name= 'all-master'),
+
+
 ]
