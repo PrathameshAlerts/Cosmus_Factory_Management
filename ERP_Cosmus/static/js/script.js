1e0bf0889f09aa5c01204224d99ac17d2ed5ad0b
PrathameshAlerts <alerts.prathamesh@gmail.com>
2024-02-07 15:22:33 +0530

    latest updated

--- ERP_Cosmus/static/js/script.js
+++ ERP_Cosmus/static/js/script.js
@@ -1,8 +1,18 @@
-function toggleSubnav(subnavId){
-    var subnav = document.getElementById(subnavId);
-    if(subnav.style.display == 'block'){
-        subnav.style.display = 'none';
-    }else{
-        subnav.style.display = 'block';
-    }
-}
+$(document).ready(function(){
+    //jquery for toggle sub menus
+    $('.sub-btn').click(function(){
+      $(this).next('.sub-menu').slideToggle();
+      $(this).find('.dropdown').toggleClass('rotate');
+    });
+
+    //jquery for expand and collapse the sidebar
+    $('.menu-btn').click(function(){
+      $('.side-bar').addClass('active');
+      $('.menu-btn').css("visibility", "hidden");
+    });
+
+    $('.close-btn').click(function(){
+      $('.side-bar').removeClass('active');
+      $('.menu-btn').css("visibility", "visible");
+    });
+  });
