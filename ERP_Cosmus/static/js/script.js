505696395e4497dbdb114021afec6692b1b30d19
PrathameshAlerts <alerts.prathamesh@gmail.com>
2024-04-11 14:33:36 +0530

    changes base page

--- ERP_Cosmus/static/js/script.js
+++ ERP_Cosmus/static/js/script.js
@@ -30,15 +30,68 @@ $(document).ready(function(){
 
   //search in using sort option
 //navbar dropdown function
-function showThePopUp(category){
+function showThePopUp(Subfield){
   var showPopup = document.getElementById("subfieldsContainer");
   showPopup.style.display = "block";
-  showPopup.innerHTML = "<h3>" + category + "</h3>";
+  showPopup.innerHTML = "<h5 class='ps-2 mt-3'>" + Subfield + "</h5>";
+  
+console.log(Subfield);
+switch (Subfield){
+  case "Main Category" :
+  showPopup.innerHTML += `<ul class="mainList"><li class="pro-head"><a href="{% url 'pproductlist' %}" class="navText">Product</a></li>
+  <li class="pro-head"> <a href="{% url 'item-list' %}" class="navText">Raw Material</a></li>
+  <li class="pro-head"> <a href="{% url 'godown-list' %}" class="navText">Godown</a></li> </ul>`;
+  break;
+  case "Sub Category" :
+  showPopup.innerHTML += `<ul class="mainList"><li class="pro-head"><a href="{% url 'simplecolorlistonly' %}" class="navText">Colors</a></li> 
+  <li class="pro-head"><a href="{% url 'item-fabgroup-list' %}" class="navText">Fabrics Groups</a></li>
+  <li class="pro-head"><a href="{% url 'unit_name-list' %}" class="navText">Units</a></li> 
+  <li class="pro-head"><a href="{% url 'gst-list' %}" class="navText">GST</a></li> 
+  <li class="pro-head"><a href="{% url 'fabric-finishes-list' %}" class="navText">Fabric Finshes</a></li>
+  <li class="pro-head"><a href="{% url 'packaging-list' %}" class="navText">Packaging List</a></li> </ul>`;
+  
 
-  function hideThePopUp(){
-    showPopup.style.display = "none";
-  }
-  setTimeout(hideThePopUp, 5000);
+  break;
+  case "Product Defination":
+  showPopup.innerHTML += `<ul class="mainList">
+  <li class="pro-head"><a href="{% url 'define-main-category-product' %}" class="navText">Main Cat Create</a></li> 
+  <li class="pro-head"><a href="{% url 'define-sub-category-product' %}" class="navText">Sub Cat Create</a></li>
+  <li class="pro-head"><a href="{% url 'product-2-subcategory' %}" class="navText">Product 2 Catagory</a></li></ul>   
+ `;
+  break;
+
+  case "Accounts" :
+    showPopup.innerHTML +=`<ul class="mainList">
+    <li class="pro-head"><a href="{% url 'account_sub_group-list' %}" class="navText">Groups</a></li> 
+           <li class="pro-head"><a href="{% url 'ledger-list' %}" class="navText">Ledger</a></li>
+           <li class="pro-head"><a href="{% url 'stock_item-list' %}" class="navText">Stock Item</a></li> 
+    </ul>`;
+    break;
+
+  case "Vouchers":
+    showPopup.innerHTML +=`<ul class="mainList">
+    <li class="pro-head"> <a href="{% url 'purchase-voucher-list' %}" class="navText">Purchase Invoice</a></li> 
+          <li class="pro-head"><a href="" class="navText">Sales Invoice</a></li>  
+          <li class="pro-head"><a href="{% url 'stock-transfer' %}" class="navText">Stock Transfer</a></li>   
+          <li class="pro-head"><a href="" class="navText">Sales Return</a></li>   
+          <li class="pro-head"><a href="" class="navText">Purchase Return</a></li>   
+          <li class="pro-head"><a href="" class="navText">Payment</a></li>   
+          <li class="pro-head"><a href="" class="navText">Reciept</a></li>   
+          <li class="pro-head"><a href="" class="navText">Journal Entry</a></li> 
+    </ul>`;
+    break;
+  case "Reports":
+    showPopup.innerHTML +=`<ul class="mainList">
+           <li class="pro-head"><a href="{% url 'account_sub_group-list' %}" class="navText">Stock Transfer Report</a></li> 
+           <li class="pro-head"><a href="#" class="navText">Purchase Report</a></li>
+           <li class="pro-head"><a href="#" class="navText">Sales Report</a></li>   
+   </ul>`;
+    break;
+
+  default:
+    break;
+}
+ 
 }
 
 //add card in form 
