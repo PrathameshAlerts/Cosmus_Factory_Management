0480912d441deef83eae8619288f002d8a96edc3
PrathameshAlerts <alerts.prathamesh@gmail.com>
2024-04-13 15:10:46 +0530

    purchase Invoice

--- ERP_Cosmus/product/views.py
+++ ERP_Cosmus/product/views.py
@@ -1572,8 +1572,10 @@ def purchasevouchercreateupdate(request, pk=None):
     return render(request,'accounts/purchase_invoice.html',context=context)
 
 
-def purchasevoucherpopup(request,unique_id,shade_id):
+def purchasevoucherpopup(request,shade_id):
 
+    formset = purchase_voucher_items_godown_formset()
+    
     try:
         godowns = Godown_raw_material.objects.all()
         item = Item_Creation.objects.get(shades__id = shade_id) 
@@ -1585,14 +1587,15 @@ def purchasevoucherpopup(request,unique_id,shade_id):
 
 
 
+
 def purchasevouchercreatepopupajax(request):
-    unique_id = request.GET.get('unique_invoice_row_id')
     shade_id = request.GET.get('selected_shade')
-    print(unique_id)
-    print(shade_id)
-    popup_url = reverse('purchase-voucher-popup', args=[unique_id,shade_id])
-    print(popup_url)
+    
+    popup_url = reverse('purchase-voucher-popup', args=[shade_id])
     return JsonResponse({'popup_url':popup_url})
+
+
+
 
 
 def purchasevoucherlist(request):
