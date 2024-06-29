b76728729968251e2ad6d7d513eeae655fd7c1de
PrathameshAlert <alerts.prathamesh@gmail.com>
2024-06-29 12:55:54 +0530

    purchase order raw material product

--- ERP_Cosmus/product/views.py
+++ ERP_Cosmus/product/views.py
@@ -29,12 +29,12 @@ from . models import (AccountGroup, AccountSubGroup, Color, Fabric_Group_Model,
                        FabricFinishes, Godown_finished_goods, Godown_raw_material,
                          Item_Creation, Ledger, MainCategory, PProduct_Creation, Product,
                            Product2SubCategory,  ProductImage, RawStockTransferMaster, StockItem,
-                             SubCategory, Unit_Name_Create, account_credit_debit_master_table,
+                             SubCategory, Unit_Name_Create, account_credit_debit_master_table, factory_employee,
                                gst, item_color_shade, item_godown_quantity_through_table,
                                  item_purchase_voucher_master, opening_shade_godown_quantity, packaging, product_2_item_through_table, purchase_order, purchase_order_for_raw_material, purchase_order_to_product, purchase_voucher_items, set_prod_item_part_name, shade_godown_items,
                                    shade_godown_items_temporary_table)
 
-from .forms import(ColorForm, CreateUserForm, CustomPProductaddFormSet,raw_material_stock_trasfer_items_formset,
+from .forms import(ColorForm, CreateUserForm, CustomPProductaddFormSet, factory_employee_form,raw_material_stock_trasfer_items_formset,
                     FabricFinishes_form, ItemFabricGroup, Itemform, LedgerForm,
                      LoginForm,OpeningShadeFormSetupdate, PProductAddForm, PProductCreateForm, ShadeFormSet,
                        StockItemForm, UnitName, account_sub_grp_form, PProductaddFormSet,
@@ -2961,6 +2961,7 @@ def purchaseorderrawmaterial(request,p_o_pk,prod_ref_no):
     product_refrence_no = prod_ref_no
     product_2_items_instances = product_2_item_through_table.objects.filter(PProduct_pk__Product__Product_Refrence_ID = product_refrence_no).order_by('Item_pk','id').distinct('Item_pk')
 
+    model_name = purchase_order_instance.product_reference_number.Model_Name
 
     physical_stock_all_godowns = {}
 
@@ -3046,12 +3047,53 @@ def purchaseorderrawmaterial(request,p_o_pk,prod_ref_no):
         
 
 
-    return render(request,'production/purchaseorderrawmaterial.html',{'form': form ,
+    return render(request,'production/purchaseorderrawmaterial.html',{'form': form ,'model_name':model_name,
                                                                       'purchase_order_raw_formset':purchase_order_raw_formset,
                                                                       'purchase_order_raw_sheet_formset':purchase_order_raw_sheet_formset,
                                                                       'physical_stock_all_godown_json':physical_stock_all_godown_json})
 
 
+
+def factory_employee_create_update(request,pk=None):
+    
+    if pk:
+        instance = get_object_or_404(factory_employee,pk=pk)
+        
+    else:
+        instance = None
+    
+    form = factory_employee_form(request.POST or None, instance =instance)
+
+    if request.method == 'POST':
+
+        if form.is_valid():
+            form.save()
+
+            return render(request,'production/factory_emp_create_update.html', {'form':form})
+
+    return render(request,'production/factory_emp_create_update.html', {'form':form})
+
+
+def factoryemplist(request):
+
+    factory_employees = factory_employee.objects.all() 
+
+    return render(request,'production/factory_emp_list.html',{'factory_employees':factory_employees})
+
+
+def factoryempdelete(request,pk=None):
+
+    try:
+        instance = get_object_or_404(factory_employee,pk-pk)
+        instance.delete()
+        messages.success(request,f' Factory Employee {instance.factory_emp_name} was deleted')
+
+    except IntegrityError as e:
+        messages.error(request,f'Cannot delete {instance.factory_emp_name} because it is referenced by other objects.')
+    return redirect('factory-emp-list')
+
+
+    
 #_________________________production-end______________________________
 
 
