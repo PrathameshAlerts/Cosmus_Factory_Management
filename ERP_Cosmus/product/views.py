7a1f7d7add9de18e7daa3b8c4847de75f94cb3d0
PrathameshAlert <alerts.prathamesh@gmail.com>
2024-07-27 15:08:20 +0530

    cutting room

--- ERP_Cosmus/product/views.py
+++ ERP_Cosmus/product/views.py
@@ -3130,7 +3130,8 @@ def purchaseorderrawmaterial(request,p_o_pk,prod_ref_no):
                             po_form_instance.process_status = '3'  # change the status to 3
                             po_form_instance.save()  # save the parent form instance 
 
-                    return(redirect(reverse('purchase-order-cutting-list',args = [purchase_order_instance.id, purchase_order_instance.product_reference_number.Product_Refrence_ID])))
+                    #return(redirect(reverse('purchase-order-cutting-list',args = [purchase_order_instance.id, purchase_order_instance.product_reference_number.Product_Refrence_ID])))
+                    return redirect('purchase-order-cutting-list-all')
                 
                 except ValueError as ve:
                     messages.error(request,f'Error Occured - {ve}')
