03b028474f997d85e7aad241704702aedec39c38
PrathameshAlert <alerts.prathamesh@gmail.com>
2024-09-14 13:53:21 +0530

    authentication starts here

--- ERP_Cosmus/core/urls.py
+++ ERP_Cosmus/core/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from .views import list_company_users, create_company_user, edit_company_user, delete_company_user,login_user, logout_user, permission_denied_view
+from .views import list_company_users, create_company_user, edit_company_user, delete_company_user,login_user, logout_user, password_change_view, permission_denied_view
 
 urlpatterns = [
     path('company/users/', list_company_users, name='list_company_users'),
@@ -10,4 +10,5 @@ urlpatterns = [
     path('permission_denied', permission_denied_view, name='permission_denied'),
     path('login/',login_user, name='login'),
     path('logout/',logout_user, name='logout'),
+    path('passwordchange/', password_change_view, name='password-change')
 ]
--- ERP_Cosmus/product/models.py
+++ ERP_Cosmus/product/models.py
@@ -306,7 +306,6 @@ class packaging(models.Model):
 
 
 
-
 class Item_Creation(models.Model):
     STATUS =  [
         ("Unused","Unused"),
@@ -774,7 +773,7 @@ class product_to_item_labour_workout(models.Model):
 class labour_workout_childs(models.Model):
     labour_workout_master_instance = models.ForeignKey(labour_workout_master, on_delete=models.PROTECT)
     challan_no = models.CharField(unique=True, null=False, blank=False)
-    labour_name = models.ForeignKey(Ledger, on_delete=models.PROTECT, null=True, blank=True)
+    labour_name = models.ForeignKey(Ledger, on_delete=models.PROTECT, null=False, blank=False)
     total_process_pcs = models.IntegerField(null = True, blank=True)
     total_balance_pcs = models.IntegerField(null = True, blank=True)
     created_date = models.DateTimeField(auto_now = True)
