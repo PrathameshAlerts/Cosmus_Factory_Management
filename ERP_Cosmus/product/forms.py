a6834823392f88f9538af40b47cddd033a40825d
PrathameshAlert <alerts.prathamesh@gmail.com>
2024-10-08 16:33:29 +0530

    labour workin update

--- ERP_Cosmus/product/forms.py
+++ ERP_Cosmus/product/forms.py
@@ -616,7 +616,7 @@ class purchase_order_raw_product_sheet_form(forms.ModelForm):
     class Meta:
         model = purchase_order_for_raw_material
 
-        fields = ['product_sku','product_color','material_name','rate','panha','units','g_total','consumption','unit_value','total_comsumption','physical_stock','balance_physical_stock']
+        fields = ['product_sku','product_color','material_name','rate','panha','units','g_total','g_total_combi','consumption','unit_value','total_comsumption','physical_stock','balance_physical_stock']
 
 
 
