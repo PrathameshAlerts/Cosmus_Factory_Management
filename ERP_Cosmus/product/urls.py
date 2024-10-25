b29936d0a873d88d189d73f8ef82aa8f9a299204
cosmussoftware <cosmussoftware@gmail.com>
2024-10-25 17:10:20 +0530

    approval popup

--- ERP_Cosmus/product/urls.py
+++ ERP_Cosmus/product/urls.py
@@ -235,7 +235,7 @@ urlpatterns = [
 
     path('godownproductreport/<int:g_id>', views.finished_goods_godown_wise_report, name = 'finished-goods-godown-wise-report'),
     path('finishedgoodsgodownproductrefwisereport/<int:ref_no>', views.finished_goods_godown_product_ref_wise_report, name = 'finished-goods-godown-product-ref-wise-report'),
-    path('finishedgoodsgodownproductrefvendorwisereport/<int:vendor_id>/<int:ref_no>', views.finished_goods_vendor_model_wise_report, name = 'finished-goods-godown-product-ref-vendor-wise-report'),
+    path('finishedgoodsgodownproductrefvendorwisereport/<int:ref_no>/<int:challan_no>', views.finished_goods_vendor_model_wise_report, name = 'finished-goods-godown-product-ref-vendor-wise-report'),
 
     path('allfinishedgoodsstockreport/', views.allfinishedgoodsstockreport, name = 'all-finished-goods-stock-report'),
 
